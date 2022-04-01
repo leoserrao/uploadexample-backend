55f311817f36ece914daea684c4fceaaa51d4c0f
Léo Serrão Barbosa <leo.barbosa@ifap.edu.br>
2022-04-01 19:30:29 -0300

    criado e configurado o multer

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const res = require('express/lib/response');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 const routes = require('express').Router();
 
-routes.get("/", (req, res) => {
+routes.post("/posts", multer(multerConfig).single('file'), (req, res) => {
+    console.log(req.file);
+    
     return res.json({ hello: "World" });
 });
 
